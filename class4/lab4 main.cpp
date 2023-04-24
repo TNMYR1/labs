744e3c07b00cafc39d5c41af63d492c00a4151c2
TNMYR1 <130778872+TNMYR1@users.noreply.github.com>
2023-04-24 17:40:57 +0500

    Add files via upload

---
+++ class4/lab4 main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Pair.h"
+#include "Hipotenuse.h"
+using namespace std;
+void main() {
+	Pair a;
+	cin>>a; 
+	cout<<a; 
+	Hipo c;//создать объект 
+	cin>>c;//ввести значения атрибутов
+	cout<<c;//вывести значения атрибутов
+}
